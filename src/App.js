c2c53420b463f29bfa23ad5cd51a5dc53f4c2d4f
themiyagun <themiya.gunasekara95@gmail.com>
2022-05-09 07:23:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import SideNav from "./components/SideNav/SideNav";
+import LoginPage from "./Pages/Login/LoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <SideNav></SideNav>
+      {/* <LoginPage></LoginPage> */}
     </div>
   );
 }
